7faa4692c8a7b5ff87951988257766e1fab7f601
thewarsawpakt <smallperson07@gmail.com>
2021-03-18 18:03:36 -0700

    added ip whitelist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, abort
 from flask.helpers import send_from_directory
-from flask import Flask, render_template
 from flask_httpauth import HTTPBasicAuth
 import os
 
@@ -16,6 +16,10 @@ USERS = {
     "username": generate_password_hash("password"),
 }
 
+ALLOWED_IPS = [
+    "127.0.0.1",
+]
+
 CONTENT_PATH = "content"
 
 HOST = "127.0.0.1"
@@ -30,19 +34,27 @@ def verify_password(username, password):
 @app.route("/")
 @auth.login_required
 def root():
-    return render_template("index.html", folders=os.listdir(CONTENT_PATH))
+    if request.remote_addr in ALLOWED_IPS:
+        return render_template("index.html", folders=os.listdir(CONTENT_PATH))
+    else:
+        abort(401)
 
 @app.route("/<directory>/")
 @auth.login_required
 def get_directory(directory):
-    try:
-        return render_template("index.html", folders=os.listdir(f"{CONTENT_PATH}/{directory}"))
-    except NotADirectoryError:
-        return send_from_directory(directory=CONTENT_PATH, filename=directory, as_attachment=True)
+    if request.remote_addr in ALLOWED_IPS:
+        try:
+            return render_template("index.html", folders=os.listdir(f"{CONTENT_PATH}/{directory}"))
+        except NotADirectoryError:
+            return send_from_directory(directory=CONTENT_PATH, filename=directory, as_attachment=True)
+    else:
+        abort(401)
 
 @app.route("/<directory>/<file>")
 @auth.login_required
 def get_directory_file(directory, file):
-    return send_from_directory(directory=os.path.join(CONTENT_PATH, directory), filename=file, as_attachment=True)
-
+    if request.remote_addr in ALLOWED_IPS:
+        return send_from_directory(directory=os.path.join(CONTENT_PATH, directory), filename=file, as_attachment=True)
+    else:
+        abort(401)
 app.run(HOST, PORT)
